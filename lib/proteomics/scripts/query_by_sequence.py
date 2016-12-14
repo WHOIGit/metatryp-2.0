39af469ff46e0913771f78f528915090c47c9094
dagaylord <dgaylord@whoi.edu>
2016-12-14 16:43:24 -0500

    removed unneded levenstein function, can do this directly in DB instead

--- lib/proteomics/scripts/query_by_sequence.py
+++ lib/proteomics/scripts/query_by_sequence.py
@@ -49,30 +49,6 @@ def main():
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.INFO)
 
-    # Define levenshtein function in SQLite.
-    try:
-        def levenshtein(s1,s2):
-            l1 = len(s1)
-            l2 = len(s2)
-            matrix = [range(l1 + 1)] * (l2 + 1)
-            for zz in range(l2 + 1):
-              matrix[zz] = range(zz,zz + l1 + 1)
-            for zz in range(0,l2):
-              for sz in range(0,l1):
-                if s1[sz] == s2[zz]:
-                  matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz])
-                else:
-                  matrix[zz+1][sz+1] = min(matrix[zz+1][sz] + 1, matrix[zz][sz+1] + 1, matrix[zz][sz] + 1)
-            return matrix[l2][l1]
-
-        connection = db.get_connection()
-        #connection.connection.create_function("LEVENSHTEIN", 2, levenshtein)
-    except Exception as e:
-        logger.exception('Could not define Levenshtein distance function: %s' % e)
-        raise e
-
-    session = db.get_session(bind=connection)
-
     # Read in sequences to query.
     sequences = []
     max_dist =   lev_dist = args.max_distance
