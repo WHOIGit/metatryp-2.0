c8f375ea8672149546a7b891032d0ef983891917
dagaylord <dgaylord@whoi.edu>
2017-04-10 14:05:52 -0400

    removed test logging and unused code

--- lib/proteomics/services/annotations.py
+++ lib/proteomics/services/annotations.py
@@ -9,11 +9,7 @@ import re
 db_filename = "C:\Users\David\Documents\WHOI Files\Metatryp\Venter\CN_2012_92pct_seqid_annotation_map_for_mak_saito.sqlite"
 
 def open_venter_db(self, db_filename):
-   # used_filename = db_filename
-   # if not os.path.exists(used_filename):
-   #     used_filename = os.path.join(self.exe_path, used_filename)
-   #     if not os.path.exists(used_filename):
-    #        sys.exit('Could not file database {}'.format(db_filename))
+
     self.annotationdb = sqlite3.connect(db_filename)
     self.annotationdb.row_factory = sqlite3.Row  # get rows as dicts
 
@@ -21,8 +17,6 @@ def open_venter_db(self, db_filename):
 def extract_venter_annotations(self, seq_ids, logger=None):
     if not seq_ids:
         return
-
-    logger.info("sequence ids: %s" % (seq_ids))
 
     query = """SELECT
         a.seq_id, a.orf_id, a.scaffold_id, a.orf_num, a.annotation,
@@ -40,8 +34,6 @@ def extract_venter_annotations(self, seq_ids, logger=None):
     cursor = self.annotationdb.cursor()
 
     cursor.execute(query, seq_ids)
-
-
     annot = []
     curr_seq_id = None
     new_seq = None
@@ -51,6 +43,7 @@ def extract_venter_annotations(self, seq_ids, logger=None):
         new_seq_id = row[0]
 
         if new_seq_id != curr_seq_id:
+
             if new_seq:
                 new_seq.append(tar_annotation)
                 annot.append(new_seq)
@@ -60,8 +53,9 @@ def extract_venter_annotations(self, seq_ids, logger=None):
 
         else:
             tar_annotation = tar_annotation + '|' +(row[-1])
-       # annot.append(new_seq)
-       # logger.info("annotation rows: %s" % (annot))
+    if new_seq:
+        new_seq.append(tar_annotation)
+        annot.append(new_seq)
 
     returnVal = [[None if x == '' else x for x in c] for c in annot]
     return returnVal
