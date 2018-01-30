3b520450091bf3c3f937efe596f0df50c60973a4
dagaylord <dgaylord@whoi.edu>
2018-01-30 15:17:24 -0500

    -added support for taxon hierachy stored as ltree in db -added additional metadata fields to import

--- lib/proteomics/scripts/update_taxons.py
+++ lib/proteomics/scripts/update_taxons.py
@@ -12,7 +12,7 @@ def main():
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.INFO)
 
-    filename = 'C:/Users/David/Projects/metatryp-2.0/data_files/NAH_microbialgenomes/170428_NAH_updated_taxontable_withlabels_for_update.csv'
+    filename = 'C:/Users/David/Projects/metatryp-2.0/data_files/NAH_microbialgenomes/171211_NAH_updated_taxontable_withlabels_update.csv'
     with open(filename, 'rb') as f:
         reader = csv.reader(f)
         try:
@@ -21,18 +21,25 @@ def main():
                 cur = db.get_psycopg2_cursor()
                 #check to see if taxon file matching the fasta file key is in database
 
-                cur.execute("select * from taxon where id = %s;", (row[15],))
+                cur.execute("select * from taxon where id = %s;", (row[16],))
                 taxon_result = cur.fetchone()
 
                 db.psycopg2_connection.commit()
                 if taxon_result is not None:
                     print row
+                    s = "."
+                    seq = (row[6], row[7], row[8], row[9],row[10], row[11], row[12])
+                    hierachy = s.join(seq)
+                    hierachy = hierachy.replace(" ", "_")
+                    print hierachy
+
                     cur = db.get_psycopg2_cursor()
-                    cur.execute("update taxon set tax_oid = %s, tax_domain = %s, status = %s, study_name = %s,"
-                                "genome_name= %s, sequencing_center= %s, img_genome_id= %s, tax_phylum= %s, tax_class= %s, "
-                                "tax_order= %s, tax_family= %s, tax_genus= %s, tax_species= %s, genome_size= %s, gene_count= %s "
-                                "where id= %s", (long(row[0]), row[1],row[2],row[3],row[4],row[5],long(row[6]),row[7],row[8],row[9],row[10],row[11],row[12],long(row[13]),long(row[14]), row[15],))
+                    cur.execute("update taxon set tax_oid = %s,  status = %s, study_name = %s,"
+                                "genome_name= %s, sequencing_center= %s, img_genome_id= %s, tax_form = %s, tax_domain = %s, tax_phylum= %s, tax_class= %s, "
+                                "tax_order= %s, tax_family= %s, tax_genus= %s, tax_species= %s, genome_size= %s, gene_count= %s, hierachy= %s, ncbi_id= %s, ncbi_url= %s, img_url= %s"
+                                "where id= %s", (long(row[0]), row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],long(row[14]),long(row[15]), hierachy, long(row[17]), row[18], row[19], row[16]))
                     db.psycopg2_connection.commit()
+
 
 
         except csv.Error as e:
