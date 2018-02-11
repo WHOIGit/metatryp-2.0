41c3e6f4742f01782d96e85d79c8ed6807d8483d
dagaylord <dgaylord@whoi.edu>
2018-02-11 11:02:03 -0500

    corrected img_url import

--- lib/proteomics/scripts/update_taxons.py
+++ lib/proteomics/scripts/update_taxons.py
@@ -37,7 +37,7 @@ def main():
                     cur.execute("update taxon set tax_oid = %s,  status = %s, study_name = %s,"
                                 "genome_name= %s, sequencing_center= %s, img_genome_id= %s, tax_form = %s, tax_domain = %s, tax_phylum= %s, tax_class= %s, "
                                 "tax_order= %s, tax_family= %s, tax_genus= %s, tax_species= %s, genome_size= %s, gene_count= %s, hierachy= %s, ncbi_id= %s, ncbi_url= %s, img_url= %s"
-                                "where id= %s", (long(row[0]), row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],long(row[14]),long(row[15]), hierachy, long(row[17]), row[18], row[19], row[16]))
+                                "where id= %s", (long(row[0]), row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],long(row[14]),long(row[15]), hierachy, long(row[17]), row[18], row[20], row[16]))
                     db.psycopg2_connection.commit()
 
 
