43970877ba5739ede9e102e2e71cf628fcd658a0
David Gaylord <dgaylord@whoi.edu>
2018-11-15 10:01:20 -0500

    converted python code to python3 using python2to3

---
+++ lib/proteomics/scripts/update_metagenome_taxons.py
@@ -0,0 +1,54 @@
+from proteomics import db
+from proteomics.models import Taxon
+
+import logging
+import csv
+import time
+
+def main():
+    start_time = time.time()
+
+    logger = logging.getLogger('update_metagenome_taxons')
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.INFO)
+
+    filename = 'C:/Users/David/Projects/metatryp-2.0/data_files/ProteOMZ/new_orf_taxa_keys.csv'
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        try:
+            count=1;
+            for row in reader:
+
+                 #print count
+                # count = count+1
+                 s = "."
+                 seq = (row[1], row[2], row[3], row[4],row[5], row[6], row[7], row[8])
+                 hierachy = s.join(seq)
+                 hierachy = hierachy.replace(" ", "_")
+                 hierachy = hierachy.replace("-", "_")
+                 hierachy = hierachy.replace("(", "_")
+                 hierachy = hierachy.replace(")", "_")
+                 hierachy = hierachy.replace("[", "")
+                 hierachy = hierachy.replace("]", "")
+                 hierachy = hierachy.replace("'", "")
+                 hierachy = hierachy.replace("'", "")
+                 #print hierachy
+
+                 cur = db.get_psycopg2_cursor()
+                 cur.execute("select mt.ncbi_id from metagenome_taxon mt where mt.ncbi_id =%s;", (row[0],))
+                 taxon_result = cur.fetchone()
+
+                 if taxon_result is None:
+                     ncbi_url = "http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id="+row[0]
+                     print(ncbi_url)
+                     cur.execute("insert into metagenome_taxon (ncbi_id, tax_group, tax_kingdom, tax_phylum, tax_class, "
+                                 "tax_order, tax_family, tax_genus, tax_species, ncbi_url, hierachy)"
+                                 " values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",  (int(row[0]), row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],ncbi_url, hierachy))
+                     db.psycopg2_connection.commit()
+
+        except csv.Error as e:
+           logger.error('file %s, line %d: %s' % (filename, reader.line_num, e))
+
+
+if __name__ == '__main__':
+    main()
---
+++ lib/proteomics/services/annotations_proteomz.py
@@ -0,0 +1,61 @@
+import sys
+import os.path
+import argparse
+import logging
+from gzip import GzipFile
+import sqlite3
+import re
+
+db_filename = "C:\\Users\David\Projects\metatryp-2.0\data_files\ProteOMZ\2018_0515_ProteOMZ_annotations.csv"
+
+def open_venter_db(self, db_filename):
+
+    self.annotationdb = sqlite3.connect(db_filename)
+    self.annotationdb.row_factory = sqlite3.Row  # get rows as dicts
+
+
+def extract_venter_annotations(self, seq_ids, logger=None):
+    if not seq_ids:
+        return
+
+    query = """SELECT
+        a.seq_id, a.orf_id, a.scaffold_id, a.orf_num, a.annotation,
+        a.gene_name, a.orf_tax_level, a.orf_taxonomy, a.orf_tax_id,
+        a.contig_tax_level, a.contig_taxonomy, a.contig_tax_id,
+        b.annotation AS target_annotation
+        FROM annotations AS a
+        JOIN annotations as b
+            ON a.scaffold_id = b.scaffold_id
+        WHERE a.seq_id in (%s)
+            AND a.orf_num -3 <= b.orf_num AND a.orf_num +3 >= b.orf_num
+        ORDER BY a.seq_id, b.orf_num""" % ','.join('?'*len(seq_ids))
+
+    open_venter_db(self, db_filename)
+    cursor = self.annotationdb.cursor()
+
+    cursor.execute(query, seq_ids)
+    annot = []
+    curr_seq_id = None
+    new_seq = None
+    tar_annotation = None
+    for row in cursor:
+
+        new_seq_id = row[0]
+
+        if new_seq_id != curr_seq_id:
+
+            if new_seq:
+                new_seq.append(tar_annotation)
+                annot.append(new_seq)
+            new_seq = list(row)[:-1]
+            curr_seq_id = row[0]
+            tar_annotation = row[-1]
+
+        else:
+            tar_annotation = tar_annotation + '|' +(row[-1])
+    if new_seq:
+        new_seq.append(tar_annotation)
+        annot.append(new_seq)
+
+    returnVal = [[None if x == '' else x for x in c] for c in annot]
+    return returnVal
