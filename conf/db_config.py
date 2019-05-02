ab382580764bd3bc3f42d853f2c9920264651fc8
David Gaylord <dgaylord@whoi.edu>
2019-05-01 22:46:36 -0400

    added stub database configuration

---
+++ conf/db_config.py
@@ -0,0 +1,10 @@
+#Keep the contents of this file out of source control.
+# There may be a better way to do this but this will work for testing.
+
+#Connection Settings for the metatryp database, need to be set for program to run
+DB_NAME=""
+DB_CONN_PREFIX="postgresql"
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_PASS=""
+DB_USER=""
