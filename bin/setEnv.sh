917aae7405420b415a3978fd63a6015e62f5c8f0
David Gaylord <dgaylord@whoi.edu>
2019-05-01 21:34:57 -0400

    updated for conda removed irrelevant old code

--- bin/setEnv.sh
+++ bin/setEnv.sh
@@ -5,12 +5,11 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 echo $BASE_DIR
 # Activate virtualenv
-source $BASE_DIR/py2.7/bin/activate
+#source $BASE_DIR/py2.7/bin/activate
+conda activate $BASE_DIR/metatryp2_env/
 
 # Set python path to include proteomics
 export PYTHONPATH="$PYTHONPATH:$BASE_DIR/lib"
 echo $PYTHONPATH
-# Set path to database
-export PROTEOMICS_DB="postgresql://metatryp:Pr0t3om1cs@localhost:5432/proteomics"
-export PROTEOMICS_DB_NAME="proteomics"
+
 
