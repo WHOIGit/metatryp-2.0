dace9fb0f9bc89ef7b932a4a1dd7ddcbf60f80f7
David Gaylord <dgaylord@whoi.edu>
2019-05-08 14:47:53 -0400

    changed to use a python script instead of a straight query

--- bin/list_taxons.sh
+++ bin/list_taxons.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/setEnv.sh
 
-psql -U protein -d $PROTEOMICS_DB_NAME -c "SELECT * FROM taxon;"
+python -m proteomics.scripts.list_taxons
 
