cd553815a42ab6d37d194da4d4415309c252f5fe
David Gaylord <dgaylord@whoi.edu>
2020-04-22 11:55:00 -0400

    removed additional un-used files

--- bin/initialize_db.sh
+++ bin/initialize_db.sh
@@ -6,6 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/setEnv.sh
 
 # Initialize the db.
-python -c "import proteomics.db as db; db.init_db()"
+#python -c "import proteomics.db as db; db.init_db()"
+
+pg_restore --dbname=tesdb2 conf/metatryp_schema.backup
 
 echo "Initialized DB at '$PROTEOMICS_DB'"
--- bin/install_requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-. $DIR/setEnv.sh
-
-pip install -r $DIR/../pip_requirements.txt
