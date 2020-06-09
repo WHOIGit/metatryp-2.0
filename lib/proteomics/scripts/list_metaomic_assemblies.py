a00cd020f34067fdf6e2cd4cbfc62f91308aa533
David Gaylord <dgaylord@whoi.edu>
2020-06-09 13:29:50 -0400

    added scripts to list meta-omic assemblies

---
+++ lib/proteomics/scripts/list_metaomic_assemblies.py
@@ -0,0 +1,21 @@
+from proteomics import config
+from proteomics import db
+import logging
+
+
+def main():
+    logger = logging.getLogger('metaomic_assemblies')
+    logger.addHandler(logging.StreamHandler())
+    logger.setLevel(logging.INFO)
+
+    cur = db.get_psycopg2_cursor();
+    cur.execute("select m.name from metagenome m;")
+
+    logger.info("Meta-omic Assemblies");
+    for record in cur:
+        logger.info("%s" % (record[0]))
+    db.psycopg2_connection.commit()
+
+if __name__ == '__main__':
+    main()
+
